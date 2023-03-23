9cae0354a3ca32819275308330e3b04db2abe9bf
Daniel Mutti <dgmutti@hotmail.com>
2023-03-23 19:54:25 +0000

    Add code

--- main.cpp
+++ main.cpp
@@ -2,59 +2,72 @@
 #include "LM75B.h"
 #include "C12832.h"
 
+// add objects for LCD
 C12832 lcd(p5, p7, p6, p8, p11);
-
 LM75B sensor(p28,p27);
+
 Serial pc(USBTX,USBRX);
 
-LocalFileSystem local("local");               // Create the local filesystem under the name "local"
+// Create the local filesystem under the name "local" on mbed
+LocalFileSystem local("local");               
 
 int main ()
 {
+    
+    // create variables for temperature readings
+    float temp;
+    float minTemp = 100.0;
+    float maxTemp = -100.0;
+    float totalTemp = 0;
+    int numReadings = 0;
 
-    //Try to open the LM75B
-    if (sensor.open()) {
-        printf("Device detected!\n");
+    // write temperature readings in the file
+    FILE *fp = fopen("/local/temperature.txt", "w");
+    
+    // for loop counter
+    for (int i=0; i<=10; i++){  // ten readings
+        // read temperature from sensor
+        temp = sensor.read();
+        
+        // write temperature to file
+        fprintf(fp, "%.2f\n", temp);
 
-        while (1) {
-            lcd.cls();
-            lcd.locate(0,3);
-            lcd.printf("Temp = %.3f\n", (float)sensor);
-            wait(1.0);
+        // minimum value
+        if( temp < minTemp ) {
+            minTemp = temp;
         }
 
-    } else {
-        error("Device not detected!\n");
+        // maximum value
+        if ( temp > maxTemp ) {
+            maxTemp = temp;
+        }
+        totalTemp += temp;
+        numReadings++;
+
+        // display temperature on the LCD
+        lcd.cls();
+        lcd.locate(0,3);
+        lcd.printf("Tempearture: %.2f C", temp);
+
+        // wait for 7 minutes = 420 seconds
+        wait(420);
+
+        // close file
+        fclose(fp);
+
+        // calculate average temperature
+        float avgTemp = totalTemp / numReadings; 
+
+        // display values on LCD
+        lcd.cls();
+
+        lcd.locate(0,0);
+        lcd.printf("Min Temp: %.2f C", minTemp);
+        lcd.locate(0,8);
+        lcd.printf("Max Temp: %.2f C", maxTemp);
+        lcd.locate(0,16);
+        lcd.printf("Avg Temp: %.2f C", avgTemp);
+   
     }
-
-}
-
-
-
-
- 
-int main() {
-    FILE *fp = fopen("/local/out.txt", "w");  // Open "out.txt" on the local file system for writing
-    fprintf(fp, "Hello World!");
-    fclose(fp);
-}
-
-
-
-
-
-
-int main()
-{
-    int j=0;
-    lcd.cls();
-    lcd.locate(0,3);
-    lcd.printf("mbed application board!");
-
-    while(true) {   // this is the third thread
-        lcd.locate(0,15);
-        lcd.printf("Counting : %d",j);
-        j++;
-        wait(1.0);
-    }
+        
 }
